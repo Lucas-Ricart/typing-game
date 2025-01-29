2d41540e2c8896e6284c51f19fead5a7acba52ab
Lucas Ricart <lucas.ricart@laplateforme.io>
2025-01-29 11:01:17 +0100

    fix: fruits throw ok
    need to add letters on the fruits

--- main.py
+++ main.py
@@ -7,30 +7,41 @@ pygame.init()
 fruits = ['melon', 'orange', 'pomegranate', 'guava', 'bomb']
 HEIGHT = 480
 WIDTH = 854
+DIMENSIONS = WIDTH, HEIGHT
 WHITE =(255, 255, 255)
 FPS = 15
 clock = pygame.time.Clock()
 pygame.display.set_caption("typing_game.py")
-gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
+gameDisplay = pygame.display.set_mode((DIMENSIONS))
+#fonts
+font = pygame.font.SysFont(None, 25)
+
+
+    
 
 
 def generate_random_fruits(fruit):
     fruit_path = "images/" + fruit + ".png"
     data[fruit] = {
         'img': pygame.image.load(fruit_path),
-        'x' : random.randint(100,500),               
-        'y' : 800,
+        'x' : random.randint(100,HEIGHT),               
+        'y' : WIDTH,
         'speed_x': random.randint(-10,10),    
         'speed_y': random.randint(-80, -60),    
         'throw': False,                       
         't': 0,                               
         'hit': False,
+        'letter':random.choice(letters)
     }
-    if random.random() >= 0.75:     
+    if random.random() >= 0.9:     
         data[fruit]['throw'] = True
     else:
         data[fruit]['throw'] = False
 data = {}
+A = 65
+letters = []
+for i in range(26) :
+    letters.append([chr(A + i)])
 for fruit in fruits:
     generate_random_fruits(fruit)
 
@@ -46,11 +57,13 @@ while running :
             value['x'] += value['speed_x']
             value['y'] += value['speed_y']
             value['speed_y'] += (1 * value['t'])
-            value['t'] += 1
+            value['t'] += 0.6
             if value['y'] <= WIDTH:
                 gameDisplay.blit(value['img'], (value['x'], value['y']))
             else:
                 generate_random_fruits(key)
+        else :
+            generate_random_fruits(key)
     pygame.display.update()
     clock.tick(FPS)
 
