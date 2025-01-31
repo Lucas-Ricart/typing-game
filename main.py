e08d829f8c458835afd6d767d98f09ba6571d51e
Lucas Ricart <lucas.ricart@laplateforme.io>
2025-01-31 13:50:50 +0100

    refactor: modify the program, and add 3 functions
    cut - draw_fruit - update_fruit_positions

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ gameDisplay = pygame.display.set_mode((DIMENSIONS))
 #fonts
 font = pygame.font.SysFont(None, 50)
 
-def generate_random_fruits(fruit):
+def spawn_fruit(fruit):
+    '''Generate fruits'''
     fruit_path = "images/" + fruit + ".png"
     data[fruit] = {
         'img': pygame.image.load(fruit_path),
@@ -30,7 +31,7 @@ def generate_random_fruits(fruit):
         'hit': False,
         'letter':random.choice(letters)
     }
-    if random.random() >= 0.995:            #spawn rate
+    if random.random() >= 0.99:            #spawn rate
         if key == 'bomb' :          #reduce bomb spawn probability
             if random.random() >= 0.5 :
                 data[fruit]['throw'] = True
@@ -41,11 +42,50 @@ def generate_random_fruits(fruit):
             data[fruit]['throw'] = True
     else:
         data[fruit]['throw'] = False
+
+def cut() :
+            if not value['hit'] and pressed == str(value['letter']).strip("[]'") :
+                if key == 'bomb' :
+                    half_fruit_path = "images/explosion.png"
+                elif key == 'ice_cube' :
+                    half_fruit_path = "images/break_ice_cube.png"
+                else :
+                    half_fruit_path = "images/" + "half_" + key + ".png"
+                value['img'] = pygame.image.load(half_fruit_path)
+                value['speed_x'] = -value['speed_x']
+                value['speed_y'] += -40
+                value['hit'] = True
+
+def draw_fruit() :
+    if value['y'] <= HEIGHT :
+        value['img'] = pygame.transform.rotate(value['img'], 90)            #rotate the fruit
+        gameDisplay.blit(value['img'], value['img'].get_rect(center=(value['x'], value['y'])))
+        rect = value['img'].get_rect(center=(value['x'], value['y']))
+        center_x = rect.left
+        center_y = rect.top
+        letter = str(value['letter']).strip("[]'")
+        letter_surface = font.render(letter, 1, WHITE)
+        gameDisplay.blit(letter_surface, letter_surface.get_rect(center=(center_x, center_y)))
+    else :
+        spawn_fruit(key)
+
+def update_fruit_positions() :
+    if value['throw']:
+        value['x'] += value['speed_x']
+        value['y'] += value['speed_y']
+        value['speed_y'] += (1 * value['gravity'])
+        value['gravity'] += 0.3             #dropping speed
+        if value['x'] <= 0 or value['x'] >= WIDTH-20 :
+            value['speed_x'] = -value['speed_x']
+        draw_fruit()
+    else:
+        spawn_fruit(key)
+    cut()
 data = {}
-letters = ['Z', 'Q', 'S', 'D']
+letters = ['Z', 'Q', 'S', 'D']          #letters to press to hit fruits
 
 for fruit in fruits:
-    generate_random_fruits(fruit)
+    spawn_fruit(fruit)
 
 running = True
 while running :
@@ -57,40 +97,10 @@ while running :
         if event.type == pygame.KEYDOWN :
             if 97 <= event.key <= 122 :
                 pressed = chr(event.key).upper()
-                print(pressed)
         
     for key, value in data.items():
-        if value['throw']:
-            value['x'] += value['speed_x']
-            value['y'] += value['speed_y']
-            value['speed_y'] += (1 * value['gravity'])
-            value['gravity'] += 0.3             #dropping speed
-            if value['x'] <= 0 or value['x'] >= WIDTH-20 :
-                value['speed_x'] = -value['speed_x']
-            if value['y'] <= HEIGHT :
-                value['img'] = pygame.transform.rotate(value['img'], 90)            #rotate the 
-                gameDisplay.blit(value['img'], value['img'].get_rect(center=(value['x'], value['y'])))
-                rect = value['img'].get_rect(center=(value['x'], value['y']))
-                center_x = rect.left
-                center_y = rect.top
-                letter = str(value['letter']).strip("[]'")
-                letter_surface = font.render(letter, 1, WHITE)
-                gameDisplay.blit(letter_surface, letter_surface.get_rect(center=(center_x, center_y)))
-            else:
-                generate_random_fruits(key)
-        else :
-            generate_random_fruits(key)
-        if not value['hit'] and pressed == str(value['letter']).strip("[]'") :
-            if key == 'bomb' :
-                half_fruit_path = "images/explosion.png"
-            elif key == 'ice_cube' :
-                half_fruit_path = "images/break_ice_cube.png"
-            else :
-                half_fruit_path = "images/" + "half_" + key + ".png"
-            value['img'] = pygame.image.load(half_fruit_path)
-            value['speed_x'] = -value['speed_x']
-            value['speed_y'] += -40
-            value['hit'] = True
+        update_fruit_positions() 
+    
     pygame.display.update()
     clock.tick(FPS)
 
